df333e818bcd969198874405d1b4cc8719c6444c
xEarthxd <di_stat@hotmail.com>
2018-08-10 18:24:08 +0700

    Test Frontend Branch First Commit

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,18 +6,21 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
     
     public function getIndex()
     {
-        $total = 0;
-        foreach (auth()->user()->knowyc->token as $key => $token) {
-            $total += $token->total_token;
-        }
-        return view('auth.balance')->withProfile(auth()->user()->knowyc)->withBalance(number_format($total, 8, '.', ','));
+
+        // $total = 0;
+        // foreach (auth()->user()->knowyc->token as $key => $token) {
+        //     $total += $token->total_token;
+        // }
+        // return view('auth.balance')->withProfile(auth()->user()->knowyc)->withBalance(number_format($total, 8, '.', ','));
+        
+        return view('index');
     }
 
     public function getTermsandconditions()
