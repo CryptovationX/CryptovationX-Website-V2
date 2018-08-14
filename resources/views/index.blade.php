1f8f56d08f07385ebfcf82fc31d60df2ba2e36cb
xEarthxd <di_stat@hotmail.com>
2018-08-14 21:45:54 +0700

    edit main.css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,8 @@
 <body>
     @include ('layout.navbar')
     @include ('page.header')
+    @include ('page.vision')
+    @include ('page.project')
     @include ('page.robot')
     @include ('page.howitworks')
     @include ('page.blog')
--- resources/views/page/header.blade.php
+++ resources/views/page/header.blade.php
@@ -1,33 +1,29 @@
 <section id="header">
     <div class="jumbotron header-bg">
-        <div id="particles-js" class="landingpage-top" ></div>
-        <div class="container mb100 mt100">
+        <div id="particles-js" class=""></div>
+        <div class="container">
             <div class="row">
-                <div class="col-sm-6 d-flex flex-column justify-content-center align-items-center mb-5 mb-sm-0">
-                    <div class="wow fadeIn " data-wow-duration=".5s">
-                        <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/logos/header-logo.png" width="100%">
-                        <h3 class="white-text">"The Best Friend for Crypto Investors"</h3>
-                        <div class="text-center">
-                            <br>
-                            <a class="btn-x" id="White Paper Top" href="{{ asset('whitepaper.pdf') }}" target="_blank" role="button">Read our White Paper</a>
-                        </div>
+                <div class="col-md-6 col-xl-6 col-sm-6 mt200">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/logos/header-logo.png" width="100%">
+                    <h3 class="white-text">"The Best Friend for Crypto Investors"</h3>
+                    <div class="text-center">
+                        <br>
+                        <a class="btn-x" id="White Paper Top" href="#" target="_blank" role="button">Read our White Paper</a>
                     </div>
+                    </p>
                 </div>
-                <div class="col-sm-6 d-flex flex-column justify-content-center align-items-center mb-5 mb-sm-0 wow fadeIn " data-wow-duration=".2s"">
-                    <a href="http://bit.ly/AD2Website" target="_Black">
-                    <div class="border-image text-center">
-                        <img class="border-image img-fluid" src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/CXA_Airdrop2.png" id="airdrop2-image"
-                            alt="Responsive image">
+                <div class="col-md-6 col-xl-6 col-sm-6 mt150 mb150">
+                    <a href="http://bit.ly/AD2Website" target="_Black"></a>
+                    <div class="border-image">
+                        <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/CXA_Airdrop2.png" id="airdrop2-image" class="img-fluid" alt="Responsive image">
                     </div>
-                    </a>
                     <br>
-                    
-                        <div class="text-center text-white">
+                    <p class="white-text">
+                        <div class="text-center">
                             <a class="btn-x" id="airdrop2-button" href="http://bit.ly/AD2Website" target="_blank" role="button">Join CXA Airdrop Now!</a>
                         </div>
-                    
                 </div>
             </div>
         </div>
     </div>
-</section>
+    </section>
