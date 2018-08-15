f473516d02776e1ffda2c24f472f3779807f782a
xEarthxd <di_stat@hotmail.com>
2018-08-15 19:30:32 +0700

    edit header

--- resources/views/page/header.blade.php
+++ resources/views/page/header.blade.php
@@ -1,29 +1,66 @@
-<section id="header">
-    <div class="jumbotron header-bg">
-        <div id="particles-js" class=""></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-xl-6 col-sm-6 mt200">
-                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/logos/header-logo.png" width="100%">
-                    <h3 class="white-text">"The Best Friend for Crypto Investors"</h3>
-                    <div class="text-center">
-                        <br>
-                        <a class="btn-x" id="White Paper Top" href="#" target="_blank" role="button">Read our White Paper</a>
-                    </div>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHRBQQH" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<!-- Begin Jumbotron -->
+
+<div class="jumbotron jumbotron-main background-white" id="homey">
+    <div id="particles-js" class="landingpage-top"></div>
+
+    <div class="container center-vertically-holder center-top top-mobile">
+        <div class="row">
+            <div class="col-md-6 col-xl-6 col-sm-6 mt-top">
+                <div class=" text-center1 main-site scaleReveal">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/logos/fulltoplogo.png" width="100%">
+                    <br>
+                    <h4 class="scaleReveal white-text2" style="text-align:center;">"The Best Friend for Crypto Investors"</h4>
+                    <h1 class="scaleReveal top-x pt-4">
+                        <p style="text-align:center;">
+                            <a class="btn btn-top" id="White Paper Top" href=" {{ asset('whitepaper.pdf') }} " target="_blank" role="button" onClick="ga('send','event','whitepaper-top','click','whitepaper.pdf','100');">Read our White Paper</a>
+                        </p>
+                    </h1>
+                    <p>
+                        <div class="active item text-center">
+                            <p>Through utilization of Artificial Intelligence (AI) CryptovationX has built a best in class Robo-advisory
+                                platform for blockchain investing in line with our Company core Vision
+                                <strong>'Wealth for All'</strong>
+                            </p>
+                            <span class="section-divider mb15"></span>
+                        </div>
                     </p>
                 </div>
-                <div class="col-md-6 col-xl-6 col-sm-6 mt150 mb150">
-                    <a href="http://bit.ly/AD2Website" target="_Black"></a>
+            </div>
+            <div class="col-md-6 col-xl-6 col-sm-6 mt-top text-center  scaleReveal">
+                <a href="https://medium.com/cryptovationx/cryptovationx-monthly-newsletter-july-2018-ed8944134388" target="_blank">
                     <div class="border-image">
-                        <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/CXA_Airdrop2.png" id="airdrop2-image" class="img-fluid" alt="Responsive image">
+                        <img class="intercon" src="/images/july2018x.jpg" id="news-image">
                     </div>
-                    <br>
-                    <p class="white-text">
-                        <div class="text-center">
-                            <a class="btn-x" id="airdrop2-button" href="http://bit.ly/AD2Website" target="_blank" role="button">Join CXA Airdrop Now!</a>
-                        </div>
-                </div>
+                </a>
+                <p>
+                    <a class="btn more-team" id="news-button" href="https://medium.com/cryptovationx/cryptovationx-monthly-newsletter-july-2018-ed8944134388"
+                        target="_blank" role="button">July 2018 Newsletter</a>
+                </p>
+                <!-- <p>
+				<a class="btn more-team22" id="hybrid-button" href="https://bx.in.th/ref/AFILaS/" target="_blank" role="button">
+				<img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/marketLogo/bxLogo.png" width="80px" height="30px">
+				</a>
+				<a class="btn more-team22" id="hybrid-button" href="https://www.binance.com/?ref=11125918" target="_blank" role="button" >
+				<img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/marketLogo/binanceLogo.png" width="80px" height="35px">
+				</a>
+			</p> -->
+
+                <a href="https://icobench.com/ico/cryptovationx" target="_blank" rel="nofollow" title="CryptovationX on ICObench">
+                    <img border="0" src="https://icobench.com/rated/cryptovationx?shape=horizontal&size=s" id="icoBench" width="188px" height="62px"
+                        class="mt-4" alt="CryptovationX ICO rating" style="border-radius:7px;" />
+                </a>
+                <a href="https://icomarks.com/ico/cryptovationx" target="_blank" rel="nofollow" title="CryptovationX">
+                    <img border="0" src="https://icomarks.com/widget/c/cryptovationx/horizontal.svg" id="icoMarks" width="200px" height="66px"
+                        class="mt-4" alt="CryptovationX ICO rating" style="border-radius:7px;" onClick="ga('send','event','icomarks','click','icomarksX','100');"
+                    />
+                </a>
             </div>
         </div>
     </div>
-    </section>
+</div>
