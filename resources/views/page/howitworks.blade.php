4a0e755c4923a4e7f29314b9da0a8009e024b340
xEarthxd <di_stat@hotmail.com>
2018-08-16 17:56:04 +0700

    add how it works

--- resources/views/page/howitworks.blade.php
+++ resources/views/page/howitworks.blade.php
@@ -1,30 +1,37 @@
-<section id="howitwork">
+<!-- Begin How it Work -->
+<div id="media">
     <div class="container section">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <h1 class="text-center mb-2">How It Works</h1>
-                <span class="section-divider mb-4"></span>
+        
+            <div class="col-xs-12 section-title text-center bottomReveal">
+                <h2>How It Works</h2>
+                <span class="section-divider mb-1"></span>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-8">
-                <h3 class="heading-1 mb-4 what">CryptovationX Features</h3>
+        
+        <div class="row mt-2">
+            <div class="col-sm-8  leftReveal">
+                <h5 class="heading-1 mb-1 what">CryptovationX</h5>
+                <ul class="section-divider2 mt-2 mb-4" role="tablist" >
+                    </ul>
                 <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/AjfWmyxhfcg" allowfullscreen="" width="300px"></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/jBrrPCC7fH4" frameborder="0" allow="autoplay; encrypted-media"
+                        allowfullscreen></iframe>
                 </div>
             </div>
-            <div class="col-sm-4 mt30-xs">
-                <h3 class="heading-1 mb20 what">What is CryptovationX?</h3>
-                <div class="tab-content tab-default tab-default">
+            <div class="col-sm-4 mt30-xs  rightReveal">
+                <h5 class="heading-1 mb-1 what">CryptovationX White Paper</h5>
+                <ul class="section-divider2 mt-2 mb-4" role="tablist" >
+                    </ul>
+               
+
+                <div class="tab-content tab-default tab-default whitepaper-border">
                     <div class="active text-small" id="whitepaper1">
                         <div class="thumbnail no-margin whitepaper">
-                            <div class="caption">
-                                <p>White Paper</p>
-                                <a class="btn-default btn-x" id="White Paper ENG" href=" {{ asset('whitepaper.pdf') }} " target="_blank" role="button">English</a>
-                                <a class="btn-x btn-default" id="White Paper CHI" href=" {{ asset('whitepaper_chi.pdf') }}" role="button">中文</a>
-                                <a class="btn-x btn-default" id="White Paper JPN" role="button" data-toggle="collapse" href="#WhitepaperJapanese" aria-expanded="false"
-                                    aria-controls="WhitepaperJapanese">日本語</a>
-                                <a class="btn-x btn-default" id="White Paper KOR" href="#" role="button">한국어</a>
+                            <img src="images/whitepaper_cover.png" alt="Work" class="whitepaper-cover text-center" >
+                            <div class="caption text-center">
+                                <a class="btn btn-default" id="White Paper ENG" href=" {{ asset('whitepaper.pdf') }} " target="_blank" role="button" style="font-size: 10px">English</a>
+                                <a class="btn btn-default" id="White Paper CHI" href=" {{ asset('whitepaper_chi.pdf') }}" target="_blank" role="button" style="font-size: 10px">中文</a>
+                                <a class="btn btn-default" id="White Paper JPN" role="button" data-toggle="collapse" href="#WhitepaperJapanese" aria-expanded="false"
+                                    aria-controls="WhitepaperJapanese" style="font-size: 10px">日本語</a>
                                 <div class="collapse jp" id="WhitepaperJapanese">
                                     <br>
                                     <p>仮想通貨クリプトアセット投資に対するロボ・アドバイザープラットフォーム
@@ -33,10 +40,10 @@
                                         <br>
                                         <br> 概要
                                         <br>
-                                        <br>   クリプトベーションＸは、クリプトベーションにより設立され、AVA Advisory及びAsia Wealth Groupの２社がビジネスパートナーとなっています。「全ての人に資産を」をモットーに、人工知能（AI）の活用により、クリプトアセット投資に最適なロボ・アドバイザリープラットフォームを立ち上げ、クリプト投資家のベストパートナーとなることを目指しています。iOS、Androidのモバイルアプリも開発予定で、６つの基本機能と、Arbi（裁定取引）、Specto（投機的取引）、Broca（ICO）の３つのロボアドバイザーをお使いいただけます。このプラットフォームは、お客様に３つのＡ、Answer（答え）、Assist（サポート）、
+                                        <br>   クリプトベーションＸは、クリプトベーションにより設立され、AVA Advisory 及び Asia Wealth Group の２社がビジネスパートナーとなっています。「全ての人に資産を」をモットーに、人工知能（AI）の活用により、クリプトアセット投資に最適なロボ・アドバイザリープラットフォームを立ち上げ、クリプト投資家のベストパートナーとなることを目指しています。iOS、Androidのモバイルアプリも開発予定で、６つの基本機能と、Arbi（裁定取引）、Specto（投機的取引）、Broca（ICO）の３つのロボアドバイザーをお使いいただけます。このプラットフォームは、お客様に３つのＡ、Answer（答え）、Assist（サポート）、
                                         Auto-investing（自動投資）をお届けします。
                                         <br>
-                                        <br> 　また、私たちは、投資家の皆様の様々な問題を解決するだけでなく、教育、失業、貧困といった社会経済問題にも目を向けています。「全ての人に資産を」イニシアティブは、①全ての人が、パフォーマンスによる損失補償が可能なアナリスト（予測ができる人）育成、及び社会貢献のためのＣＳＲ活動の実施といった金融教育メカニズム、②年次のコンペを開催し、新たな人材とイノベーションを発掘し、Ｒ＆Ｄ奨励と、エコシステムに関する知識の共有を目的とする、ブロックチェーンと人工知能エコシステムの協働、この２つの戦略を以って、社会に貢献していきます。
+                                        <br>　また、私たちは、投資家の皆様の様々な問題を解決するだけでなく、教育、失業、貧困といった社会経済問題にも目を向けています。「全ての人に資産を」イニシアティブは、①全ての人が、パフォーマンスによる損失補償が可能なアナリスト（予測ができる人）育成、及び社会貢献のためのＣＳＲ活動の実施といった金融教育メカニズム、②年次のコンペを開催し、新たな人材とイノベーションを発掘し、Ｒ＆Ｄ奨励と、エコシステムに関する知識の共有を目的とする、ブロックチェーンと人工知能エコシステムの協働、この２つの戦略を以って、社会に貢献していきます。
                                     </p>
                                 </div>
                             </div>
@@ -45,4 +52,5 @@
                 </div>
             </div>
         </div>
-</section>
+    </div>
+</div>
