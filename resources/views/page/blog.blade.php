80b2d1869a371ed0bdb3eda48bc2a2c3ff809788
xEarthxd <di_stat@hotmail.com>
2018-08-20 17:39:37 +0700

    add Blog

--- resources/views/page/blog.blade.php
+++ resources/views/page/blog.blade.php
@@ -1,57 +1,115 @@
-<section class="container" id="blog">
-    <div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 text-center mb20 ">
+<!-- Begin Blog -->
+<div id="work" class="section-padding-top mt50">
+    <div class="container container-fluid ">
+        
+            <div class="col-xs-12 section-title text-center ">
                 <h2>Blog</h2>
-                <span class="section-divider"></span> 
+                <span class="section-divider mb15 mt20"></span>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/CXA_Dropair.png" class="img-fluid" alt="Responsive image">
-                <a href="https://medium.com/cryptovationx/cryptovationx-is-launching-an-ico-called-cxa-9a94acb7c0ee" target="_blank">
-                </a>
-                <p class="mt-2 text-center">CryptovationX is launching an ICO called ‘CXA’</p>
+        
+        <div id="thework">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 portfolio-box no-padding " data-my-order="3">
+                <div class="portfolio-image-holder leftReveal">
+                    <img src="https://cdn-images-1.medium.com/max/1600/1*Q6TMxC1yvs7HoFqoqu49Sw.jpeg" alt="1" class="img-responsive portfolio-image image-holder" width="100%">
+                </div>
+                <span class="portfolio-hover blog-news">
+                    <span class="">
+                        <a href="https://medium.com/cryptovationx/sec-thailand-launched-the-3rd-fintech-challenge-program-and-visited-cryptovationx-office-at-kx-2bbe0a1bdf66"
+                            id="medium" target="_blank">
+                            <span class="portfolio-links">
+                                <i class="icon ion-ios-arrow-forward portfolio-links-icons"></i>
+                            </span>
+                        </a>
+                        <span class="project-title no-margin-bottom mt-1"></span>
+                    </span>
+                </span>
+                <p class="text-blog">SEC Thailand launched the 3rd Fintech Challenge program and visited CryptovationX office at KX center.</p>
             </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/News.jpg" class="img-fluid" alt="Responsive image">
-                <a href="https://medium.com/cryptovationx/cryptovationx-media-coverage-2753991f29df" target="_blank">
-                </a>
-                <p class="mt-2 text-center">CryptovationX: Media Coverage</p>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 portfolio-box no-padding graphics print" data-my-order="9">
+                <div class="portfolio-image-holder rightReveal">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/News.jpg" alt="7" class="img-responsive portfolio image-holder" width="100%">
+                </div>
+                <span class="portfolio-hover">
+                    <span>
+                        <a href="https://medium.com/cryptovationx/cryptovationx-media-coverage-2753991f29df" target="_blank">
+                            <span class="portfolio-links">
+                                <i class="icon ion-ios-arrow-forward portfolio-links-icons"></i>
+                            </span>
+                        </a>
+                        <span class="project-title no-margin-bottom mt-1"></span>
+                    </span>
+                </span>
+                <p class="text-blog">CryptovationX: Media Coverage</p>
             </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/journey.jpg" class="img-fluid" alt="Responsive image">
-                <a href="https://medium.com/cryptovationx/cryptovationx-journey-review-60465754bdd4" target="_blank">
-                </a>
-                <p class="mt-2 text-center">CryptovationX: Journey Review</p>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 portfolio-box no-padding graphics" data-my-order="5">
+                <div class="portfolio-image-holder bottomReveal">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/journey.jpg" alt="4" class="img-responsive portfolio-image image-holder" width="100%">
+                </div>
+                <span class="portfolio-hover">
+                    <span>
+                        <a href="https://medium.com/cryptovationx/cryptovationx-journey-review-60465754bdd4" target="_blank">
+                            <span class="portfolio-links">
+                                <i class="icon ion-ios-arrow-forward portfolio-links-icons"></i>
+                            </span>
+                        </a>
+                        <span class="project-title no-margin-bottom mt-1"></span>
+                    </span>
+                </span>
+                <p class="text-blog">CryptovationX: Journey Review</p>
             </div>
-
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/partner.jpg" class="img-fluid" alt="Responsive image">
-                <a href="https://medium.com/cryptovationx/cryptovationx-partnership-journey-2018-27c475ed4095" target="_blank">
-                </a>
-                <p class="mt-2 text-center">CryptovationX Partnership Journey: 2018</p>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 portfolio-box no-padding graphics print" data-my-order="9">
+                <div class="portfolio-image-holder rightReveal">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/partner.jpg" alt="7" class="img-responsive portfolio-image image-holder" width="100%">
+                </div>
+                <span class="portfolio-hover">
+                    <span>
+                        <a href="https://medium.com/cryptovationx/cryptovationx-partnership-journey-2018-27c475ed4095" target="_blank">
+                            <span class="portfolio-links">
+                                <i class="icon ion-ios-arrow-forward portfolio-links-icons"></i>
+                            </span>
+                        </a>
+                        <span class="project-title no-margin-bottom mt-1"></span>
+                    </span>
+                </span>
+                <p class="text-blog">CryptovationX Partnership Journey: 2018</p>
             </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 ">
-                <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/demoday.jpg" class="img-fluid" alt="Responsive image">
-                <a href="https://medium.com/cryptovationx/monthly-newsletter-may-2018-25a7e9848724" target="_blank">
-                </a>
-                <p class="mt-2 text-center">Monthly Newsletter: May 2018</p>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 portfolio-box no-padding graphics print" data-my-order="9">
+                <div class="portfolio-image-holder rightReveal">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/demoday.jpg" alt="7" class="img-responsive portfolio-image image-holder" width="100%">
+                </div>
+                <span class="portfolio-hover">
+                    <span>
+                        <a href="https://medium.com/cryptovationx/monthly-newsletter-may-2018-25a7e9848724" target="_blank">
+                            <span class="portfolio-links">
+                                <i class="icon ion-ios-arrow-forward portfolio-links-icons"></i>
+                            </span>
+                        </a>
+                        <span class="project-title no-margin-bottom mt-1"></span>
+                    </span>
+                </span>
+                <p class="text-blog">Monthly Newsletter: May 2018</p>
             </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4" class="img-responsive portfolio-image">
-                <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/zb.jpg" class="img-fluid" alt="Responsive image">
-                <a href="https://medium.com/cryptovationx/cryptovationx-team-was-invited-to-zbth-x-cryptonist-crypto-blockchain-event-b1637c5923b7" target="_blank">
-                </a>
-                <p class="mt-2 text-center">CryptovationX team was invited to ZBTH x CRYPTONIST Crypto & Blockchain Event</p>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 portfolio-box no-padding graphics print" data-my-order="9">
+                <div class="portfolio-image-holder rightReveal">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/cryptovationx/public/work/zb.jpg" alt="7" class="img-responsive portfolio-image image-holder" width="100%">
+                </div>
+                <span class="portfolio-hover">
+                    <span>
+                        <a href="https://medium.com/cryptovationx/cryptovationx-team-was-invited-to-zbth-x-cryptonist-crypto-blockchain-event-b1637c5923b7"
+                            target="_blank">
+                            <span class="portfolio-links">
+                                <i class="icon ion-ios-arrow-forward portfolio-links-icons"></i>
+                            </span>
+                        </a>
+                        <span class="project-title no-margin-bottom mt-1"></span>
+                    </span>
+                </span>
+                <p class="text-blog">CryptovationX team was invited to ZBTH x CRYPTONIST Crypto & Blockchain Event</p>
             </div>
         </div>
     </div>
-    <p>
-        <a class="btn btn-default btn-x" href="https://medium.com/cryptovationx" target="_blank" role="button">View All</a>
+    <p style="text-align:center;">
+        <a class="btn btnv-all mt-3" id="White Paper Top" href="https://medium.com/cryptovationx" target="_blank"
+            role="button">View All</a>
     </p>
-</section>
+</div>
